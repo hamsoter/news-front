69b1a1fff9e19bbbfafa06c3f7911d270fdb7ced
nyanfas <omilzmil0w0@gmail.com>
2023-09-14 20:41:14 +0900

    :lipstick: 더보기 => a tag

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,18 +1,24 @@
-import React, { ReactNode } from "react";
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 
 type Props = {
-  children: ReactNode;
+  children: string;
 };
 
 function NewsItem({ children }: Props) {
+  const splitBase = "더보기 ▶ "; // 문자열을 자르는 기준
+  const content = children.split(splitBase)[0];
+  const href = children.split(splitBase)[1];
+
   return (
     <Typography
       variant="body1"
       fontFamily={"Pretendard-Regular"}
       sx={{ color: "#121212", padding: 2 }}
     >
-      {children}
+      {content}
+      <Link href={href} sx={{ mx: 0.5 }}>
+        자세히 보기
+      </Link>
     </Typography>
   );
 }
