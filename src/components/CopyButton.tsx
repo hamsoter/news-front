2341458f3493852278842a8ee096224db1e42965
nyanfas <omilzmil0w0@gmail.com>
2023-09-14 21:49:59 +0900

    :sparkles: 복사시 alert message 띄우기

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Snackbar } from "@mui/material";
 import CopyAllIcon from "@mui/icons-material/CopyAll";
 
 type Props = {
@@ -7,16 +7,40 @@ type Props = {
 };
 
 function CopyButton({ content }: Props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <Button
-      variant="outlined"
-      sx={{ marginX: 0.5 }}
-      onClick={() => {
-        window.navigator.clipboard.writeText(content);
-      }}
-    >
-      전체 복사<CopyAllIcon fontSize="small"></CopyAllIcon>
-    </Button>
+    <>
+      <Button
+        variant="outlined"
+        sx={{ marginX: 0.5 }}
+        onClick={() => {
+          window.navigator.clipboard.writeText(content);
+          setOpen(true);
+        }}
+      >
+        전체 복사<CopyAllIcon fontSize="small"></CopyAllIcon>
+      </Button>
+      <Snackbar
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+        open={open}
+        autoHideDuration={1000}
+        onClose={handleClose}
+        ContentProps={{
+          sx: { background: "#34a746" },
+        }}
+        sx={{ mb: "100px" }}
+        message="복사 완료!"
+      />
+    </>
   );
 }
 
